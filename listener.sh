23e7c319b2fda42d72b143832aadf8a69a4da72d
polmes <pol.developer1@gmail.com>
2016-10-09 08:11:02 +0200

    fixed directory path

--- listener.sh
+++ listener.sh
@@ -1,4 +1,4 @@
-FILES=$(echo $(cut -f2 crispy_info) | tr '\n' ' ')
+FILES=$(echo $(cut -f2 ~/.crispy/crispy_info) | tr '\n' ' ')
 echo "Watching:" $FILES
 while true; do
 	inotifywait -qq $FILES # -e modify,attrib,close_write,move,create,delete
