158d45f7f4eb69fd5c915764076b0e69096fbfbe
polmes <pol.developer1@gmail.com>
2016-10-09 09:12:52 +0200

    listener for sync

--- listener.sh
+++ listener.sh
@@ -1,6 +1,8 @@
-FILES=$(echo $(cut -f2 ~/.crispy/crispy_info) | tr '\n' ' ')
+#!/bin/bash
+FILES=$(echo $(echo $(cut -f2 ~/.crispy/crispy_info) | tr '\n' ' ') | sed 's/~/$HOME/g')
+FILES=$(eval echo $FILES)
 echo "Watching:" $FILES
 while true; do
 	inotifywait -qq $FILES # -e modify,attrib,close_write,move,create,delete
-	echo "sync"
+	./crispy sync 
 done
