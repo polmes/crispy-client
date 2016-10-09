5bf0718f1a6738922a6533651f3b312168a88abd
polmes <pol.developer1@gmail.com>
2016-10-09 08:05:54 +0200

    mv old files to server

--- listener.js
+++
@@ -1,4 +0,0 @@
-var source = new EventSource( 'https://dev.coderagora.com/crispy/stream.php' );
-source.addEventListener( 'message', function(e) {
-	console.log( e.data );
-}, false );
--- wait.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-inotifywait -m -r -e modify,attrib,close_write,move,create,delete ~/syncTest/ && echo "something new"
